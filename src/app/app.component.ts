a5084a41eb608664ace54fb4d9367e4c57b064bb
Med Taher Ghazel <119009624+MedTaherGhazel@users.noreply.github.com>
2023-08-12 15:20:37 +0100

    nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vkgym';
+  isDropdownOpen = false;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
 }
